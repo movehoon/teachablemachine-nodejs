a4dde3c8f3d5fe4be6a638ab455f15a6851414ae
Donghoon Lee <movehoon@gmail.com>
2022-12-29 16:44:55 +0900

    Support teachable machine pose

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const canvas = require('canvas');
 require('@tensorflow/tfjs'); // Load before @teachablemachine/image
 const tmImage = require('@teachablemachine/image')
+const tmPose = require('@teachablemachine/pose')
 const express = require('express');
 const formData = require('express-form-data');
 const os = require('os');
@@ -35,6 +36,7 @@ app.listen(3000, () => {
 });
 
 let models = {}
+let models_pose = {}
 
 async function loadModel(URL) {
 
@@ -124,4 +126,88 @@ function _arrayBufferToBase64( buffer ) {
         binary += String.fromCharCode( bytes[ i ] );
     }
     return window.btoa( binary );
+}
+
+
+// ----------------
+// ----- Pose -----
+// ----------------
+
+async function loadModelPose(URL) {
+    console.log('loadModelPose')
+    if (models_pose[URL] != undefined) {
+        delete models_pose[URL]
+    }
+    console.log('load model pose from ', URL);
+    const modelURL = URL + 'model.json';
+    const metadataURL = URL + 'metadata.json';
+    models_pose[URL] = await tmPose.load(modelURL, metadataURL);
+    console.log('model loaded ', URL);
+}
+
+app.post('/load_pose', (req, res, next) => {
+    let url = req.body['url']
+    console.log('load pose ', url)
+    loadModelPose(url)
+    res.send('load' + url)
+})
+
+
+app.post('/predict_pose', (req, res, next) => {
+    console.log('/predict_pose rereq.body ', req.body)
+    // console.log('req.body["url"] ', req.body['url'])
+    // console.log('req.body["image"] ', req.body['image'])
+    let url = req.body['url']
+    let image = req.body['image']
+    // console.log('url', url)
+    // console.log('path', image['path'])
+    fs.readFile(image['path'], function(err, data) {
+        if (err) throw err;
+        // console.log(data);
+
+        if (models_pose[url] != undefined) {
+            try {
+                getPredictionPose(models_pose[url], _arrayBufferToBase64(data), (output) => {
+                    var prb = 0
+                    var idx = 0
+                    var cls = 'NOT_FOUND'
+                    for (var i=0; i<output.length; i++) {
+                        console.log('output: ', i, ' => ', output[i])
+                        if (output[i]['probability'] > prb) {
+                            prb = output[i]['probability']
+                            cls = output[i]['className']
+                            idx = i
+                        }
+                    }
+                    console.log('prb: ', prb)
+                    console.log('class: ', cls)
+                    console.log('index: ', idx)
+                    res.send(cls)
+                });
+            }
+            catch (error) {
+                res.send('Error on processing')
+            }
+        }
+        else {
+            loadModelPose(url)
+            res.send('Model pose not loaded')
+        }
+    });
+});
+
+async function getPredictionPose(model, data, fu) {
+    const can = canvas.createCanvas(64, 64);
+    const ctx = can.getContext('2d');
+
+    const img = new canvas.Image();
+    img.onload = async () => {
+        ctx.drawImage(img, 0, 0, 64, 64);
+        const { pose, posenetOutput } = await model.estimatePose(can)
+        const prediction = await model.predict(posenetOutput);
+        console.log(prediction);
+        fu(prediction);
+    }
+    img.onerror = err => { throw err; }
+    img.src = "data:image/png;base64," + data;
 }
